c02d2850fc15ad7473a9c2e0c42d5161e527ec14
Bruno Dias <and.coffee.code@gmail.com>
2014-11-17 18:33:59 +0000

    fixed test runner. added a regexp to filter valid tests.

--- tests.php
+++ tests.php
@@ -3,13 +3,14 @@
 $current_dir = dirname(__FILE__);
 require $current_dir . '/vendor/autoload.php';
 
-
 $test_dir = $current_dir . '/tests';
 $files = array_diff(scandir($test_dir), array('..', '.'));
 
+$test_pattern = '/^test_\w+.php$/';
+
 // TODO: use a regular expression to filter valid tests files.
 foreach($files as $test) {
-  if ($test[0] != '.') {
-    require ($test_dir . '/' . $test);
+  if (preg_match($test_pattern, $test)) {
+    require $test_dir . '/' . $test;
   }
 }
