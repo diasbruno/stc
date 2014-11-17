b513e520bd4dede36cf22ab730e45023e353e4fc
Bruno Dias <and.coffee.code@gmail.com>
2014-11-17 20:03:13 +0000

    removed TODO.

--- tests.php
+++ tests.php
@@ -8,7 +8,6 @@ $files = array_diff(scandir($test_dir), array('..', '.'));
 
 $test_pattern = '/^test_\w+.php$/';
 
-// TODO: use a regular expression to filter valid tests files.
 foreach($files as $test) {
   if (preg_match($test_pattern, $test)) {
     require $test_dir . '/' . $test;
