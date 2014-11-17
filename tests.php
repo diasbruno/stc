3c68d1135e617030b5e95a7af8984fbae1cf8bdb
Bruno Dias <and.coffee.code@gmail.com>
2014-11-17 18:13:23 +0000

    exclude vim temporary files from tests.
    TODO: use a regular expression to filter valid tests.

--- tests.php
+++ tests.php
@@ -7,7 +7,9 @@ require $current_dir . '/vendor/autoload.php';
 $test_dir = $current_dir . '/tests';
 $files = array_diff(scandir($test_dir), array('..', '.'));
 
+// TODO: use a regular expression to filter valid tests files.
 foreach($files as $test) {
-  $test = require ($test_dir . '/' . $test);
-	$test();
+  if ($test[0] != '.') {
+    require ($test_dir . '/' . $test);
+  }
 }
